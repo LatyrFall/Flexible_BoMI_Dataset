b0e1e02b6f347f8997ccb619b90210c44fcbc6ee
LatyrFall <latyr.fall@gmail.com>
2018-06-03 07:18:05 -0400

    Final commit

--- dataset_postprocessed/iav.m
+++
@@ -1,9 +0,0 @@
-function iavout = iav( data )
-    size = length(data);
-    sum = 0;
-    for t = 1:size
-        sum = sum + abs(data(t));
-    end
-    iavout = sum;
-end
-
--- dataset_postprocessed/plotConfMat/example.m
+++
@@ -1,5 +0,0 @@
-clear; close all; clc;
-
-confmat = magic(3); % sample data
-% plotting
-plotConfMat(confmat, {'Dog', 'Cat', 'Horse'});
